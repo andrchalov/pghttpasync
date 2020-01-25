66695c943cf959aa6c86d736f33a8e4903819a50
Andrey Chalov <andrchalov@gmail.com>
2020-01-25 15:16:12 +0300

    Fix job callback

--- sql/functional/job/after_action.sql
+++ sql/functional/job/after_action.sql
@@ -5,26 +5,6 @@ CREATE OR REPLACE FUNCTION pghttpasync.job_after_action()
   LANGUAGE plpgsql
 AS $function$
 BEGIN
-  IF TG_OP = 'UPDATE' THEN
-    IF NEW.complete NOTNULL AND OLD.complete ISNULL THEN
-      IF NEW.callback NOTNULL THEN
-        DECALRE
-          e_message text;
-        BEGIN
-          EXECUTE NEW.callback,
-            USING json_build_object(
-              'status', NEW.response_status,
-              'body', NEW.response_body,
-              'response_headers', NEW.response_headers
-            );
-        EXCEPTION WHEN OTHERS THEN
-          GET STACKED DIAGNOSTICS NEW.error = MESSAGE_TEXT;
-          NEW.error = 'Callback failed: '||NEW.error;
-        END;
-      END IF;
-    END IF;
-  END IF;
-
   RETURN NEW;
 END;
 $function$;
--- sql/functional/job/before_action.sql
+++ sql/functional/job/before_action.sql
@@ -9,6 +9,22 @@ BEGIN
     PERFORM pg_notify('pghttpasync', NEW.id::text);
   END IF;
 
+  IF TG_OP = 'UPDATE' THEN
+    IF NEW.complete NOTNULL AND OLD.complete ISNULL THEN
+      IF NEW.callback NOTNULL THEN
+        DECLARE
+          e_message text;
+        BEGIN
+          EXECUTE NEW.callback
+            USING NEW.response_status, NEW.response_body, NEW.response_headers;
+        EXCEPTION WHEN OTHERS THEN
+          GET STACKED DIAGNOSTICS NEW.error = MESSAGE_TEXT;
+          NEW.error = 'Callback failed: '||NEW.error;
+        END;
+      END IF;
+    END IF;
+  END IF;
+
   RETURN NEW;
 END;
 $function$;
